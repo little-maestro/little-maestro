329017dcbc9f8cf802d2417a9ca1eaa773767659
primasilapuntcu <primasilapuntcu@gmail.com>
2025-05-15 21:43:10 +0700

    Create test_sound.py

---
+++ test_sound.py
@@ -0,0 +1,25 @@
+
+import time
+import pygame
+import os
+
+def play_note(note):
+    file_name = f"{note}.mp3"
+    instrument_folder = "piano" + "_directory"
+    file_path = os.path.join("little_maestro",instrument_folder, file_name)
+        
+    if os.path.exists(file_path):
+        pygame.mixer.Sound(file_path).play()
+        print(f"[INFO] Playing: Piano - {note}")
+        time.sleep(0.5)  # Adjust delay based on tempo
+
+    else:
+        raise ValueError(f"play_note, Error: {file_path} not found!")
+    
+
+level = 1
+file_path = os.path.join("little_maestro",f"level-{level}.mp3")
+if os.path.exists(file_path):
+    pygame.mixer.Sound(file_path).play()
+    time.sleep(2)
+play_note("C4")
