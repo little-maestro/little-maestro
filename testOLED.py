d78eae5879c4fd4cdae05d1704ac9d3708e32b8d
nneuyk <nneuyk@gmail.com>
2025-05-19 16:31:07 +0700

    Update testOLED.py

--- testOLED.py
+++ testOLED.py
@@ -7,6 +7,6 @@ serial = i2c(port=1, address=0x3C)  # Common I2C address
 device = ssd1306(serial, width=128, height=64)
 
 # Load and display one image
-image_path = "/home/prim/little-maestro/pixelfroggy-01.png"  # or test.bmp
+image_path = "/home/prim/Downloads/pixelfroggy-01.png" 
 image = Image.open(image_path).resize((128, 64)).convert("1")
 device.display(image)
