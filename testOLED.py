debe8fb471f028bf51b304a87372882d94415b06
nneuyk <nneuyk@gmail.com>
2025-05-20 14:22:38 +0700

    vsd

--- testOLED.py
+++ testOLED.py
@@ -3,10 +3,11 @@ from luma.oled.device import ssd1306
 from PIL import Image
 
 # Initialize I2C and OLED
-serial = i2c(port=1, address=0x3C)  # Common I2C address
+serial = i2c(port=1, address=0x3C)
 device = ssd1306(serial, width=128, height=64)
 
-# Load and display one image
-image_path = "/home/prim/Downloads/pixelfroggy-01.png" 
+# Load and display image
+image_path = "/home/prim/Downloads/pixelfroggy-01.png"
 image = Image.open(image_path).resize((128, 64)).convert("1")
-device.display(image)
+device.image(image)
+device.show()
